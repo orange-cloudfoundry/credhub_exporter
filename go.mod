790a475b1e253fbf0c6ab19fd43a976c2dfb65ae
Xavier MARCELET <xavier.marcelet@orange.com>
2020-04-06 16:35:06 +0200

    upgrade golang dependencies

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/orange-cloudfoundry/credhub_exporter
+
+go 1.14
+
+require (
+	code.cloudfoundry.org/credhub-cli v0.0.0-20200325195429-1faf152db5a4
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/prometheus/client_golang v1.5.1
+	github.com/prometheus/common v0.9.1
+	github.com/prometheus/procfs v0.0.11 // indirect
+	github.com/sirupsen/logrus v1.5.0 // indirect
+	golang.org/x/crypto v0.0.0-20200403201458-baeed622b8d8 // indirect
+	golang.org/x/sys v0.0.0-20200406113430-c6e801f48ba2 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
--- vendor/code.cloudfoundry.org/credhub-cli/credhub/request.go
+++ vendor/code.cloudfoundry.org/credhub-cli/credhub/request.go
@@ -3,10 +3,13 @@ package credhub
 import (
 	"bytes"
 	"encoding/json"
-	"io"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"os"
 )
 
 // Request sends an authenticated request to the CredHub server.
@@ -49,7 +52,15 @@ func (ch *CredHub) request(client requester, method string, pathStr string, quer
 
 	req.Header.Set("Content-Type", "application/json")
 
+	if os.Getenv("CREDHUB_DEBUG") == "true" {
+		dumpRequest(req)
+	}
+
 	resp, err := client.Do(req)
+
+	if os.Getenv("CREDHUB_DEBUG") == "true" {
+		dumpResponse(resp)
+	}
 
 	if err != nil {
 		return resp, err
@@ -67,17 +78,41 @@ func (ch *CredHub) request(client requester, method string, pathStr string, quer
 func (ch *CredHub) checkForServerError(resp *http.Response) error {
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
 		defer resp.Body.Close()
-		defer io.Copy(ioutil.Discard, resp.Body)
-		dec := json.NewDecoder(resp.Body)
-
-		respErr := &Error{}
-
-		if err := dec.Decode(respErr); err != nil {
-			return err
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return errors.New("The response body could not be read: " + err.Error())
 		}
 
+		var respErr error
+
+		switch resp.StatusCode {
+		case http.StatusNotFound:
+			respErr = &NotFoundError{}
+		default:
+			respErr = &Error{}
+		}
+
+		if err := json.Unmarshal(body, &respErr); err != nil {
+			return errors.New("The response body could not be decoded: " + err.Error())
+		}
 		return respErr
 	}
 
 	return nil
+}
+
+func dumpRequest(req *http.Request) {
+	dump, err := httputil.DumpRequestOut(req, true)
+	if err != nil {
+		fmt.Println("[DEBUG] An error occurred during request dump.", err.Error())
+	}
+	fmt.Println("[DEBUG]", string(dump))
+}
+
+func dumpResponse(resp *http.Response) {
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		fmt.Println("[DEBUG] An error occurred during response dump.", err.Error())
+	}
+	fmt.Println("[DEBUG]", string(dump))
 }
