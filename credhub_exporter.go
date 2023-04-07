c80470421acd745cc4d3e7f1c961a76cde569c34
kaly7434 <alexandre.loiseau@orange.com>
2023-04-07 22:38:56 +0200

    update go version and bump dependancies

--- credhub_exporter.go
+++ credhub_exporter.go
@@ -8,7 +8,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 	"github.com/prometheus/common/version"
-	"gopkg.in/alecthomas/kingpin.v2"
+	"github.com/alecthomas/kingpin/v2"
 	"io/ioutil"
 	"net/http"
 	"os"
